5aeb7afe1980de797227f47fdc73825a9afd1514
Richard Vong <richard.vong192@gmail.com>
2024-01-02 20:00:13 +1100

    Created MovieCard Component and Dynamically rendering movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import './App.css';
 import SearchIcon from './search.svg'
+import MovieCard from './MovieCard'
 
 const API_URL = 'http://www.omdbapi.com?apikey=e19e19ce'
 
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
-
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -33,28 +28,30 @@ const App = () => {
             <div className='search'>
                 <input
                 placeholder='Search for movies'
-                value='Superman'
-                onChange={() => {}}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                 src={SearchIcon}
                 alt='search'
-                onClick={() => {}}
+                onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
-            <div className='container'>
-                <div className='movie'>
-                    <div>
-                        <p>{movie1.Year}</p>
+{/* Dynamically looping over our movies array, which is fetched from an API.
+Taking each individually movie and dynamically passing it as a prop to our movie card. */}
+            {movies?.length > 0
+                ? (
+                    <div className='container'>
+                        {movies.map((movie) => 
+                        <MovieCard movie={movie}/>
+                        )}
                     </div>
-
-                    <div>
-                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
                     </div>
-
-                </div>
-            </div>
+                )}
         </div>
     );
 }
